ac873a8eca34195d8d8f5e2c5b3569c61f2e803d
Clive Crous <clive@star120.co.za>
2008-08-01 12:40:35 +0200

    Fix November issue

--- test/months.rb
+++ test/months.rb
@@ -7,4 +7,8 @@ describe 'Temporal math with months' do
     ( Time.local( 1975, 8, 1 ) + 5.months ).should.equal( Time.local( 1976, 1, 1 ) )
   end
 
+  it 'should not wrap years while adding 1 month to november' do
+    ( Time.local( 1975, 11, 1 ) + 1.month ).should.equal( Time.local( 1975, 12, 1 ) )
+  end
+
 end
