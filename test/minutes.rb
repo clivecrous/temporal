2764c14541e73b3ab0010e387360af14ef8a5fd9
Clive Crous <clive@crous.co.za>
2008-08-11 21:35:22 +0200

    Add unit tests for minutes to other unit comparisons

---
+++ test/minutes.rb
@@ -0,0 +1,17 @@
+require 'temporal'
+
+describe 'One minute in other units' do
+  it 'is 60 seconds' do
+    1.minute.should.equal( 60.seconds )
+  end
+  it 'is 1/60th of an hour' do
+    1.minute.should.equal( (1.0/60).hours )
+  end
+  it 'is 1/1440th of a day' do
+    1.minute.should.equal( (1.0/1440).days )
+  end
+  it 'is 1/10080th of a week' do
+    1.minute.should.equal( (1.0/10080).weeks )
+  end
+end
+
